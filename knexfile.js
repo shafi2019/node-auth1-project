36cedb96f0365ceadcee9b59ac68ad82043d8e6e
Shafi Masoumi <masoumi2017@icloud.com>
2020-02-13 17:48:33 -0800

    knexfile and db configured

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,20 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './database/shafiAuth.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA forein_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
-
 };
